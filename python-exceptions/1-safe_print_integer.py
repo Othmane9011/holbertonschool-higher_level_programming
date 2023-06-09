35cd4e9623aad3a493c4d08f6f1f2e9c7a058793
Othmane9011 <5650@holbertonstudents.com>
2023-06-09 08:46:59 -0700

    task 4

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    try:
-        print("{:d}".format(value))
-    except Exception as e:
-        return  
-    else:
-        return True
+    while True:
+        try:
+            print("{:d}".format(value))
+            return True
+        except TypeError:
+            return False
+        except ValueError:
+            return False
