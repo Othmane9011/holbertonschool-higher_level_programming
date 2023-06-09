4aacc9630e0f725f99ada111375811f574065f38
Othmane9011 <5650@holbertonstudents.com>
2023-06-09 08:39:27 -0700

    task2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,11 +4,12 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
-        except IndexError:
-            raise
-        except:
+        except ValueError:
+            pass
+
+        except TypeError:
             pass
         else:
             count += 1
     print()
-    return count
+    return count 
