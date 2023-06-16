54cafcf790517bc028f42c5487fde260715445d4
Othmane9011 <5650@holbertonstudents.com>
2023-06-16 08:25:43 -0700

    task5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
 def text_indentation(text):
-    if not isinstance(text, str):
-        raise TypeError('text must be a string')
-    ponctuation = [".", "?", ":"]
-
+    """ insert doble jump line after . : or ? """
+    str_error = "text must be a string"
     new_text = ""
-    line = ""
-    for i in text:
-        line += i
-        if i in punctuation:
-            new_text += line.strip() + "\n\n"
-            line = ""
-
-    if line:
-        new_text += line.strip()
-
-    print(new_text)
+    flag = False
+    if type(text) is not str:
+        raise TypeError(str_error)
+    new_text = text.replace(". ", ".")
+    new_text = new_text.replace(": ", ":")
+    new_text = new_text.replace("? ", "?")
+    for char in new_text:
+        if char in [".", "?", ":"]:
+            print(char)
+            print()
+            flag = True
+        else:
+            if flag is False:
+                print(char, end="")
+            else:
+                if char != " ":
+                    print(char, end="")
+                    flag = False
