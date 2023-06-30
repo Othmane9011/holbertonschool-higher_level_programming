6785c81291d57c9fb505a2b1878675379b246a96
Othmane9011 <5650@holbertonstudents.com>
2023-06-29 21:58:04 -0700

    test

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -1,19 +1,61 @@
-# tests/test_models/test_base.py
+#!/usr/bin/python3
+
+import os
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
-class TestBase(unittest.TestCase):
-    def test_id_autoincrement(self):
+
+class TestBase_instantiation(unittest.TestCase):
+    """Unittests for testing instantiation of the Base class."""
+
+    def test_no_arg(self):
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b1.id, b2.id - 1)
+
+    def test_three_bases(self):
         b1 = Base()
         b2 = Base()
         b3 = Base()
-        self.assertEqual(b1.id, 1)
-        self.assertEqual(b2.id, 2)
-        self.assertEqual(b3.id, 3)
+        self.assertEqual(b1.id, b3.id - 2)
 
-    def test_id_manual(self):
+    def test_None_id(self):
+        """ TestBase_instantiation """
+        b1 = Base(None)
+        b2 = Base(None)
+        self.assertEqual(b1.id, b2.id - 1)
+
+    def test_unique_id(self):
+        self.assertEqual(12, Base(12).id)
+
+    def test_nb_instances_after_unique_id(self):
+        b1 = Base()
+        b2 = Base(12)
+        b3 = Base()
+        self.assertEqual(b1.id, b3.id - 1)
+
+    def test_id_public(self):
         b = Base(12)
-        self.assertEqual(b.id, 12)
+        b.id = 15
+        self.assertEqual(15, b.id)
 
-if __name__ == "__main__":
-    unittest.main()
+    def test_nb_instances_private(self):
+        with self.assertRaises(AttributeError):
+            print(Base(12).__nb_instances)
+
+    def test_str_id(self):
+                self.assertEqual("hello", Base("hello").id)
+
+    def test_float_id(self):
+        self.assertEqual(5.5, Base(5.5).id)
+
+    def test_complex_id(self):
+        self.assertEqual(complex(5), Base(complex(5)).id)
+
+    def test_dict_id(self):
+        self.assertEqual({"a": 1, "b": 2}, Base({"a": 1, "b": 2}).id)
+
+    def test_bool_id(self):
+        self.assertEqual(True, Base(True).id)
