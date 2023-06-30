e393bb78e67882c250df6932780b20f1ba0ad502
Othmane9011 <124583251+Othmane9011@users.noreply.github.com>
2023-06-30 19:10:48 +0200

    Update square.py

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -21,22 +21,24 @@ class Square(Rectangle):
     @size.setter
     def size(self, value):
         """size"""
-        self.width = value 
+        self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
         """def update attribut"""
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+        if args:
 
-        if len(args) >= 1:
-            self.id = args[0]
-        if len(args) >= 2:
-            self.size = args[1]
-        if len(args) >= 3:
-            self.x = args[2]
-        if len(args) >= 4:
-            self.y = args[3]
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
 
     def to_dictionary(self):
         """dictionary"""
