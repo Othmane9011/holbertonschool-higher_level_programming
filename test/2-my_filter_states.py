d7f0177060a6fb29571cc0b8275318106a0f4176
Othmane9011 <5650@holbertonstudents.com>
2023-08-03 11:12:04 -0700

    remove

--- test/2-my_filter_states.py
+++ test/2-my_filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" script that lists all states from the database hbtn_0e_0_usa """
+""" script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument """
 
 import MySQLdb
 import sys
@@ -9,7 +10,8 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE BINARY name = '{}'\
+                ORDER BY id ASC".format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
